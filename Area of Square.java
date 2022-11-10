42a4c8453c41dc063b09abf6ccbe4623c9291a7d
SurbhiAgrawal9 <111246726+SurbhiAgrawal9@users.noreply.github.com>
2022-11-10 19:03:48 +0530

    Add files via upload

---
+++ Area of Square.java
@@ -0,0 +1,16 @@
+import java.io.*; 
+import java.util.*;
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+
+    public static void main (String[] sun) {
+    
+                      // Your code here
+                       Scanner sc = new Scanner (System.in);
+                      int side = sc.nextInt(); // 1st side n
+                      int area = side*side*9; //2*2=4
+                      System.out.print(area);
+    }
+}
