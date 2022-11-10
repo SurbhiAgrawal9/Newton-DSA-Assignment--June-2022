61df9a99bbb42dbb39695bf2e6f98a9d8e130b0a
SurbhiAgrawal9 <111246726+SurbhiAgrawal9@users.noreply.github.com>
2022-11-10 19:27:55 +0530

    Add files via upload

---
+++ Basic Of Programing[Syntax] Input,Output,Variables & O -Post Class/String Typecast.java
@@ -0,0 +1,9 @@
+static String checkConevrtion(int a)
+ {
+       // your code herestatic String checkConevrtion(int a)
+ {
+       // your code here
+       String s = Integer.toString(a);
+       return s;
+    }
+    }
