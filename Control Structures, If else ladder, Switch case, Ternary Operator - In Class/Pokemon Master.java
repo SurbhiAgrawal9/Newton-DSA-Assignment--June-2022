50fb37c89db65b7f7d52521f74ab12357f722272
SurbhiAgrawal9 <111246726+SurbhiAgrawal9@users.noreply.github.com>
2022-11-13 00:47:44 +0530

    Add files via upload

---
+++ Control Structures, If else ladder, Switch case, Ternary Operator - In Class/Pokemon Master.java
@@ -0,0 +1,27 @@
+// Problem Statement
+// Sara has A Pokeball with her and there are B pokemon in front of Sara. Considering each pokemon takes one Pokeball, your task is to tell Sara if she can catch all the pokemon or not.
+// Input
+// User Task:
+// Since this will be a functional problem, you don't have to take input. You just have to complete the function PokemonMaster() that takes integers A and B as arguments.
+
+// Constraints:-
+// 1 <= A, B <= 8
+// Output
+// Return 1 if Sara can catch all the pokemon else return 0.
+// Example
+// Sample Input:-
+// 4 3
+
+// Sample Output:-
+// 1
+
+
+
+
+
+
+static int PokemonMaster(int A, int B){
+    //Enter your code here
+    int k = (A >= B) ? 1 : 0;
+            return k;
+    }
